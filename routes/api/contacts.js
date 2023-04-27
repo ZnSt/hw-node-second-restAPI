1815ec3fcfc11ed24181304bf42d72cfe6c7cb94
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2023-04-27 21:51:10 +0300

    add

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,23 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'done' });
+  res.json({ message: 'finished' });
 });
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'done' });
+  res.json({ message: 'finished' });
 });
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'done' });
+  res.json({ message: 'finished' });
 });
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'done' });
+  res.json({ message: 'finished' });
 });
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'done' });
+  res.json({ message: 'finished' });
 });
 
 module.exports = router;
