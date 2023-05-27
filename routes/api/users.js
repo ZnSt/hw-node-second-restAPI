9dfb80099206bbdbd4b1edaa1b839e68884d1a22
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2023-05-27 20:07:50 +0300

    add secure

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const getCurrentController = require('../../controllers/getCurrent-controllers');
 const updateController = require('../../controllers/updateAvatar-controller');
+const verifyToken = require('../../controllers/verifyEmail-controllers');
 const auth = require('../../middleware/auth');
 const upload = require('../../middleware/upload');
 
 router.get('/current', auth, getCurrentController.getCurrent);
 router.patch('/avatars', auth, upload.single('avatar'), updateController.updateAvatar);
+router.get('/verify/:verificationToken', verifyToken.verifyEmail);
 
 module.exports = router;
